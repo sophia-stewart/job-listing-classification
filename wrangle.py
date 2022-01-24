e42cdb394411f52921d6eb7d52211dd6648d5249
Sophia Stewart <sophia.marcell.stewart@gmail.com>
2022-01-24 10:37:45 -0600

    Added work

--- wrangle.py
+++ wrangle.py
@@ -50,3 +50,6 @@ def split_data(df, target):
     train, test = train_test_split(df, test_size=0.2, random_state=123, stratify=df[target])
     train, validate = train_test_split(train, test_size=0.3, random_state=123, stratify=train[target])
     return train, validate, test
+
+def get_descriptions():
+    return pd.read_csv('fake_job_postings.csv')[['description', 'fraudulent']].dropna()
